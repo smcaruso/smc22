0bac29905bac4e5c113713a0651c6b5e5654f687
Steven M. Caruso <51963719+smcaruso@users.noreply.github.com>
2022-03-18 18:22:21 -0400

    project copy
    edits to project copy in json files, added parsing function to add to html doc for seo crawling

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# smc22
- 2022 studio website
+# smcaruso.com 2022 Update
+    New for 2022:
+    3D environment
+    2D text presentation
+    Object-oriented project storage
--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import featuredProjects from "./featured.json";
+import moreProjects from "./moreprojects.json";
 
-console.log(featuredProjects)
+// parse project JSON into HTML for SEO:
+
+let featuredProjectSection = document.querySelector(".featuredprojects");
+let moreProjectsSection = document.querySelector(".moreprojects");
+
+featuredProjects.forEach(function(each) { PopulateProjetText(each, featuredProjectSection)});
+moreProjects.forEach(function(each) { PopulateProjetText(each, moreProjectsSection)});
+
+function PopulateProjetText(project, section) {
+
+    let projectEntry = document.createElement("div");
+    projectEntry.classList.add("featuredproject");
+
+    let projectTitle = document.createElement("h4");
+    projectTitle.innerText = project.title;
+
+    let projectSubtitle = document.createElement("h5");
+    projectSubtitle.innerText = project.subtitle;
+
+    let projectDescription = document.createElement("p");
+    projectDescription.innerText = project.description;
+
+    section.append(projectTitle, projectSubtitle, projectDescription);
+    section.append(projectEntry);
+    
+}
